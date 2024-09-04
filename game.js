709c89c2fd2f8225a1cbaffeec4eb442cd4d076d
TheDrifter408 <nurullahan408@gmail.com>
2024-09-04 10:58:20 +0600

    added a function that returns rock, paper or scissors randomly

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+function getComputerChoice(){
+    const choice = {
+        1:"rock",
+        2:"paper",
+        3:"scissors"
+    }
+    let number = Math.floor((Math.random() * 3) + 1);
+    return choice[number];
+}
+
+console.log(getComputerChoice());
