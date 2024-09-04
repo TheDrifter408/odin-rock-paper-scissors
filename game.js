89088cf1b251bdee86e6cbd2933b47707e7a78fb
TheDrifter408 <nurullahan408@gmail.com>
2024-09-04 11:34:56 +0600

    finished fully impleting all the different choices and added functionality for 5 rounds.

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+let computerScore = 0;
+let humanScore = 0;
 function getComputerChoice(){
     const choice = {
         1:"rock",
@@ -8,4 +10,46 @@ function getComputerChoice(){
     return choice[number];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let input = prompt("Please Enter Rock,Paper or Scissors:")
+    return input;
+}
+
+function playGame(){
+    for(let i = 0;i < 5;i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection,computerSelection);
+    }
+}
+
+
+
+function playRound(humanChoice, computerChoice){
+    let human = humanChoice.toLowerCase();
+    let computer = computerChoice.toLowerCase();
+    if(human === "paper" && computer === "rock"){
+        humanScore++;
+        console.log("You Win! Paper beats Rock");
+    } else if( human === "paper" && computer === "scissors" ){
+        computerScore++;
+        console.log("You Lose! Scissor beats Paper");
+
+    } else if ( human === "rock" && computer === "paper" ){
+        computerScore++;
+        console.log("You Lose! Paper beats rock")
+    } else if( human === "rock" && computer === "scissors"){
+        humanScore++;
+        console.log("You Win! Rock beats Scissors");
+    } else if( human === "scissors" && computer === "rock"){
+        computerScore++;
+        console.log("You Lose! Rock beats Scissors");
+    } else if(human === "scissors" && computer === "paper"){
+        humanScore++;
+        console.log("You Win! Scissors beat Paper");
+    } else if(  human === computer ) {
+        console.log("Its a Draw!");
+    } 
+}
+
+playGame();
