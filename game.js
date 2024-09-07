f651ff52fa663bdff1e341172b89221061dd431c
TheDrifter408 <nurullahan408@gmail.com>
2024-09-07 13:43:13 +0600

    Created the buttons for players to select their choices

--- game.js
+++ game.js
@@ -1,5 +1,17 @@
 let computerScore = 0;
 let humanScore = 0;
+
+let mainElement = document.querySelector("#main");
+// Creating the three choices
+let rock = document.createElement('button');
+let paper = document.createElement('button');
+let scissors = document.createElement('button');
+// Crating event listeners for the buttons
+rock.addEventListener('click',playRound('rock',getComputerChoice));
+paper.addEventListener('click',playRound('paper',getComputerChoice));
+scissors.addEventListener('click',playRound('scissors',getComputerChoice));
+
+
 function getComputerChoice(){
     const choice = {
         1:"rock",
